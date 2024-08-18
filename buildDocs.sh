a44f48fdf61215502279056856577834548fe963
Sean Marquez <capsulecorplab@gmail.com>
2024-08-18 08:01:18 +0000

    remove podman from buildDocs.sh

--- buildDocs.sh
+++ buildDocs.sh
@@ -2,7 +2,7 @@
 
 # Install dof-helpers/node_modules, if not already installed
 if [ ! -r ./dof-helpers/node_modules ]; then
-    podman run --rm -v $PWD:/src -w /src docker.io/node bash -c 'cd dof-helpers && npm ci'
+    cd dof-helpers && npm ci
 fi
 
 # Make dist/ directory, if none exists
@@ -12,7 +12,7 @@ fi
 
 # generate dist/component.yaml
 echo "generating dist/component.yaml..."
-podman run --rm -v $PWD:/src -w /src docker.io/node node dof-helpers/parseComponent.js
+node dof-helpers/parseComponent.js
 
 # copy source/images/ directory to dist/
 echo "copy source/images/ directory to dist/..."
@@ -20,12 +20,12 @@ cp -r source/images/ dist/
 
 # generate dist/assemblyInstructions.adoc
 echo "generating dist/assemblyInstructions.adoc..."
-podman run --rm -v $PWD:/src -w /src docker.io/node node dof-helpers/generateAssemblyInstructions.js
+node dof-helpers/generateAssemblyInstructions.js
 
 # generate dist/assemblyInstructions.html
 echo "generating dist/assemblyInstructions.html..."
-podman run --rm --volume $PWD:/src -w "/src" docker.io/asciidoctor/docker-asciidoctor asciidoctor dist/assemblyInstructions.adoc -o dist/assemblyInstructions.html
+asciidoctor dist/assemblyInstructions.adoc -o dist/assemblyInstructions.html
 
 # generate dist/assemblyInstructions.pdf
 echo "generating dist/assemblyInstructions.pdf..."
-podman run --rm --volume $PWD:/src -w "/src" docker.io/asciidoctor/docker-asciidoctor asciidoctor dist/assemblyInstructions.adoc -o dist/assemblyInstructions.pdf -r asciidoctor-pdf -b pdf
+asciidoctor dist/assemblyInstructions.adoc -o dist/assemblyInstructions.pdf -r asciidoctor-pdf -b pdf
