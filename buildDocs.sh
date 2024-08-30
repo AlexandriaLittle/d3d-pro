9c1c7cb1b339e2cefc074b8c598e12618d65cce1
Alexandria Little <60404243+AlexandriaLittle@users.noreply.github.com>
2024-08-30 00:03:12 +0000

    Add code to deploy docs to github pages

--- buildDocs.sh
+++ buildDocs.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+#### set environment variable for project root ####
+project_root=$PWD
+
+
 # Install dof-helpers/node_modules, if not already installed
 if [ ! -r ./dof-helpers/node_modules ]; then
     cd dof-helpers && npm ci
@@ -23,9 +27,50 @@ echo "generating dist/assemblyInstructions.adoc..."
 node dof-helpers/generateAssemblyInstructions.js
 
 # generate dist/assemblyInstructions.html
-echo "generating dist/assemblyInstructions.html..."
-asciidoctor dist/assemblyInstructions.adoc -o dist/assemblyInstructions.html
+clitool="asciidoctor"
+cmdargs="assemblyInstructions.adoc -o dist/assemblyInstructions.html"
+cmd="$clitool $cmdargs"
+workdir=$project_root/dist
+podmancmd="podman run --rm -v "$workdir:/src" -w "/src" docker.io/asciidoctor/docker-asciidoctor:1.27.0 $cmd"
+condition="$clitool --version | grep $version"
+
+if ! eval $condition; then
+    echo "asciidoctor $version not installed"
+    echo "generating manual as pdf via podman..."
+    cd $project_root
+    eval $(echo $podmancmd)
+else
+    echo "generating manual as pdf..."
+    cd $workdir
+    eval $cmd
+    cd $project_root
+fi
 
 # generate dist/assemblyInstructions.pdf
-echo "generating dist/assemblyInstructions.pdf..."
-asciidoctor dist/assemblyInstructions.adoc -o dist/assemblyInstructions.pdf -r asciidoctor-pdf -b pdf -a pdf-theme=theme.yml
+clitool="asciidoctor"
+cmdargs="assemblyInstructions.adoc -o dist/assemblyInstructions.pdf -r asciidoctor-pdf -b pdf -a pdf-theme=theme.yml"
+cmd="$clitool $cmdargs"
+workdir=$project_root/dist
+podmancmd="podman run --rm -v "$workdir:/src" -w "/src" docker.io/asciidoctor/docker-asciidoctor:1.27.0 $cmd"
+condition="$clitool --version | grep $version"
+
+if ! eval $condition; then
+    echo "asciidoctor $version not installed"
+    echo "generating manual as pdf via podman..."
+    cd $project_root
+    eval $(echo $podmancmd)
+else
+    echo "generating manual as pdf..."
+    cd $workdir
+    eval $cmd
+    cd $project_root
+fi
+
+
+# generate dist/assemblyInstructions.html
+#echo "generating dist/assemblyInstructions.html..."
+#asciidoctor dist/assemblyInstructions.adoc -o dist/assemblyInstructions.html
+
+# generate dist/assemblyInstructions.pdf
+#echo "generating dist/assemblyInstructions.pdf..."
+#asciidoctor dist/assemblyInstructions.adoc -o dist/assemblyInstructions.pdf -r asciidoctor-pdf -b pdf -a pdf-theme=theme.yml
